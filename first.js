4e36934241fe2bb3d9ed105ba6a3af84d9ce215c
Zain <zainsarfraz82@gmail.com>
2024-01-09 20:14:40 +0500

    first

---
+++ first.js
@@ -0,0 +1,43 @@
+console.log("Welcome toPJS");
+console.log("Hello World")
+
+//DATA TYPES
+name = "Danish";    //string
+age = 24;           //int, number
+isCnic = flase;    // Boolean true & false 
+x = undefined;     // kch ana hai
+y = null;          // Empty
+
+console.table(name , age , icCnic , x ,y);
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(isCnic))
+console.log(typeof(x))
+console.log(typeof(y))
+
+//TWOP TYPES OF DATA
+
+//PRIMITIVE String , Int , number , booLean etc
+//NON PRIMITIVE Array , Function , objects etc
+
+// THREE TYPES OF DECELERATION
+//var can be redeclared & updated
+// let can't be redeclared but it can be updated
+//const can't be redeclared & updated
+
+var city = "karachi";
+console.log(city);
+city = "Lahore";
+console.log(city);
+city = "Islamabad";
+console.log(city);
+
+let Myage = 15;
+console.log(Myage);
+Myage =80;
+
+const pie = 3.14;
+console.log(pie);
+pie = 1040
+console.log(pie);
+
