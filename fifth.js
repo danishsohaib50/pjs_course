9c9390be5a21047cd82540a368381c6b4a84bafb
asp <asp@aptech.com>
2024-01-16 20:00:08 +0500

    vc

---
+++ fifth.js
@@ -0,0 +1,28 @@
+//array collection of data
+let heroes = ["super man" , "bat man" , "spider man" , "hulk" , "iron man"] ;
+console.log(heroes);
+console.log(heroes[3]);
+
+//array methods
+//lost index me add krta ha
+console.log(heroes.push("acha man"));
+console.log(heroes.length);
+console.log(heroes);
+//last index ko remove krta ha
+console.log(heroes.pop());
+console.log(heroes);
+
+//shift element ko remove krta h
+console.log(heroes.shift());
+//unshift element ko add krta h
+console.log(heroes.unshift("why man"));
+console.log(heroes);
+
+//slice
+console.log(heroes.slice(1,4));
+
+
+
+
+
+
---
+++ slice.js
@@ -0,0 +1,11 @@
+let fruits = ["apple" , "mango" , "orange" , "pineapple" , "kiwi" , "cherry"]
+console.log(fruits);
+
+console.log(fruits.slice(1,2));
+console.log(fruits);
+//["apple" , "mango" , "orange" , "pineapple" , "kiwi" , "cherry"]
+console.log(fruits.splice(3, 1, "lychee" , "banana"));
+console.log(fruits);
+
+
+
